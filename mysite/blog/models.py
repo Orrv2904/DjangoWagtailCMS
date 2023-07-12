e2a1b39ffcfe494a9c1843ed621c8253065f5e75
Orrv2904 <Omarrv2904@gmail.com>
2023-07-11 20:46:35 -0600

    end

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,13 +11,25 @@ class BlogIndexPage(Page):
         FieldPanel('intro', classname="full")
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', blank=True, null=True
+    )
+    caption = models.CharField(blank=True, max_length=250)
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        FieldPanel('image'),
+        FieldPanel('caption'),
     ]
