523580bafb3ec8237550bb97b69c72c45022cccd
Orrv2904 <Omarrv2904@gmail.com>
2023-07-11 19:44:04 -0600

    updated blog view

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
